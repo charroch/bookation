476868f37b23b04db0c3d79d47b482c7ee4135ea
Carl Harroch <carl@novoda.com>
2010-09-10 20:31:35 +0100

    inital commit

---
+++ android/bookation/src/novoda/bookation/Landing.java
@@ -0,0 +1,15 @@
+package novoda.bookation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Landing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
